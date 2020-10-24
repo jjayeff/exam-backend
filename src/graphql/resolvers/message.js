2044bc9fe7250f23f7ec208e03a8936c88ed02a0
jjayeff <ekkawit.lertkrai@gmail.com>
2020-10-24 14:54:05 +0700

    [UPDATE] Create project

--- src/graphql/resolvers/message.js
+++ src/graphql/resolvers/message.js
@@ -1,27 +1,33 @@
-import set from 'lodash/set';
-import get from 'lodash/get';
-import uuid from 'uuid/v1';
+import { pubsub } from '../pubsub';
+import { v4 as uuid_v4 } from 'uuid';
+import Message from '../../model/message';
 
-import mock from '../../mock';
+const NEW_MESSAGE = 'NEW_MESSAGE';
 
 export default {
   Query: {
-    messages: (parent, { roomName }) => {
-      const msgs = get(mock, `rooms.${roomName}.messages`, []);
-      return msgs;
-    }
+    messages: async (parent, { roomName }) => {
+      return await Message(roomName).find();
+    },
   },
   Mutation: {
-    sendMessage: (parent, { roomName, message }) => {
-      set(mock, `rooms.${roomName}`, {
-        messages: [
-          ...get(mock, `rooms.${roomName}.messages`, []),
-          { id: uuid(), body: message }
-        ]
+    sendMessage: async (parent, { roomName, input }) => {
+      const { body, from } = input;
+      await Message(roomName).create({
+        id: uuid_v4(),
+        body,
+        createAt: new Date(),
+        from,
       });
+      await pubsub.publish(NEW_MESSAGE, { newMessage: roomName });
       return {
-        successful: true
+        successful: true,
       };
-    }
-  }
+    },
+  },
+  Subscription: {
+    newMessage: {
+      subscribe: () => pubsub.asyncIterator([NEW_MESSAGE]),
+    },
+  },
 };
